5a1f6570ca152e2538a450d829cd6236a0e930d5
Faith <pfaithmtan@gmail.com>
2019-11-18 15:25:39 -0800

    Handle GET request of specific restaurant

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const controllers = require('../db/dbMethods.js');
 
 const app = express();
 const port = 8080;
@@ -8,6 +9,6 @@ const port = 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/restaurant/:id', controllers.getRestaurant);
 
 app.listen(port, () => console.log(`Restaurant toilet reviews app listening on port ${port}!`));
