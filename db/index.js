94d4f6e9a670c4179da1147ab83ae5695de6c752
Faith <pfaithmtan@gmail.com>
2019-11-17 17:16:32 -0800

    Create connection
    - Include database creation in connection path
    - Define schema
    - Create Model

---
+++ db/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/restaurant_toilets', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    // we're connected!
+    console.log('connected!')
+});
+
+const restaurantSchema = new mongoose.Schema({
+    id: { type: Number, unique: true },
+    name: String,
+    address: String,
+    toiletRating: Number,
+    toiletReview: String
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+
