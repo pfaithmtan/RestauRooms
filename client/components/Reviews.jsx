f1dd9f2d743a295fe1b176338d083ca7c9a86464
Faith <pfaithmtan@gmail.com>
2019-11-19 12:05:16 -0800

    Import styling on Reviews div

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { ReviewsDiv } from '../styling/style.jsx'
 
-const Reviews = (props) => (
-    <div>
-        <h2>Bathroom Reviews</h2>
-        {
-            props.toiletReviews.map(review => <div>
-                <div>{review.review}</div>
-                <div>{review.rating}</div>
-            </div>)
-        }
-    </div>
-);
+const Reviews = (props) => {
+    console.log('REVIEWS PROPS:', props);
+
+    return (
+        <ReviewsDiv>
+            <h2>Bathroom Reviews</h2>
+            {
+                props.toiletReviews.map(review => <div>
+                    <div>{review.review}</div>
+                    <div>{review.rating}</div>
+                </div>)
+            }
+            <form >
+                <label>
+                    Add a review!
+              <input name="review" value='some review' />
+                </label>
+                <input type="submit" value="Add" />
+            </form>
+        </ReviewsDiv>
+    );
+
+}
 
 export default Reviews;
