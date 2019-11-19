e404792c8cdecc8a3c252d2497701a4033b3ac82
Faith <pfaithmtan@gmail.com>
2019-11-19 15:17:21 -0800

    Handle separate clicks

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -6,16 +6,85 @@ class Reviews extends React.Component {
         super(props);
 
         this.state = {
-            review: ''
+            review: '',
+            rating1: false,
+            rating2: false,
+            rating3: false,
+            rating4: false,
+            rating5: false,
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick1 = this.handleClick1.bind(this);
+        this.handleClick2 = this.handleClick2.bind(this);
+        this.handleClick3 = this.handleClick3.bind(this);
+        this.handleClick4 = this.handleClick4.bind(this);
+        this.handleClick5 = this.handleClick5.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
         this.setState({
             review: event.target.value
+        });
+    }
+
+    // handleClick(event) {
+    //     event.preventDefault();
+    //     console.log('The link was clicked.', event.target.name);
+    //     this.setState({
+    //         [event.target.name]: ![event.target.name]
+    //     });
+    //   }
+
+    handleClick1(event) {
+        event.preventDefault();
+        this.setState({
+            rating1: true,
+            rating2: false,
+            rating3: false,
+            rating4: false,
+            rating5: false,
+        });
+    }
+    handleClick2(event) {
+        event.preventDefault();
+        this.setState({
+            rating1: false,
+            rating2: true,
+            rating3: false,
+            rating4: false,
+            rating5: false,
+        });
+    }
+    handleClick3(event) {
+        event.preventDefault();
+        this.setState({
+            rating1: false,
+            rating2: false,
+            rating3: true,
+            rating4: false,
+            rating5: false,
+        });
+    }
+    handleClick4(event) {
+        event.preventDefault();
+        this.setState({
+            rating1: false,
+            rating2: false,
+            rating3: false,
+            rating4: true,
+            rating5: false,
+        });
+    }
+    handleClick5(event) {
+        event.preventDefault();
+        this.setState({
+            rating1: false,
+            rating2: false,
+            rating3: false,
+            rating4: false,
+            rating5: true,
         });
     }
 
@@ -28,7 +97,7 @@ class Reviews extends React.Component {
     }
 
     render() {
-        const { review } = this.state;
+        const { review, rating1, rating2, rating3, rating4, rating5 } = this.state;
 
         return (
             <ReviewsDiv>
@@ -45,16 +114,16 @@ class Reviews extends React.Component {
                       <input type="text" value={review} onChange={this.handleChange} />
                     </label>
                     <p>Add a rating!</p>
-                        <input type="radio" name="rating" value="1" />1
-                        <input type="radio" name="rating" value="2" />2
-                        <input type="radio" name="rating" value="3" />3
-                        <input type="radio" name="rating" value="4" />4
-                        <input type="radio" name="rating" value="5" />5
+                    <input type="radio" name="rating1" value="1" checked={rating1} onClick={this.handleClick1} />1
+                        <input type="radio" name="rating2" value="2" checked={rating2} onClick={this.handleClick2} />2
+                        <input type="radio" name="rating3" value="3" checked={rating3} onClick={this.handleClick3} />3
+                        <input type="radio" name="rating4" value="4" checked={rating4} onClick={this.handleClick4} />4
+                        <input type="radio" name="rating5" value="5" checked={rating5} onClick={this.handleClick5} />5
                     <input type="submit" value="Add" />
                 </form>
             </ReviewsDiv>
-                            );
-                        }
-                    }
-                    
-                    export default Reviews;
+        );
+    }
+}
+
+export default Reviews;
