71288b3c594b7691068e29cf6acacce9dd063b2b
Faith <pfaithmtan@gmail.com>
2019-11-19 10:52:24 -0800

    Render specific restaurant's toilet reviews

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,12 +5,50 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            name: '',
+            address: '',
+            toiletReviews: []
+        };
+    }
+
+    componentDidMount() {
+        this.getRestaurantToiletReview();
+    }
+
+    getRestaurantToiletReview() {
+        axios.get(`/api${window.location.pathname}`)
+            .then((response) => {
+                console.log(response.data);
+                this.setState({
+                    name: response.data[0].name,
+                    address: response.data[0].address,
+                    toiletReviews: response.data[0].toiletReviews
+                })
+            })
+            .catch((error) => {
+                console.log('Oh no! An error!');
+                console.log(error);
+            })
     }
 
     render() {
+        const { name, address, toiletReviews } = this.state;
+
         return (
-            <div>HELLO THERE!</div>
+            <div>
+                <div> <h1> {name} </h1> </div>
+                <div> <h3> {address} </h3> </div>
+                <div>Maybe search bar for certain reviews</div>
+                <div>
+                    {
+                        toiletReviews.map(review => <div>
+                            <div>{review.review}</div>
+                            <div>{review.rating}</div>
+                        </div>)
+                    }
+                </div>
+            </div>
         );
     }
 }
