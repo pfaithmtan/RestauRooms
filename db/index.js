38df1856eba0a6626fd0640dfd13ce7af6084aa0
Faith <pfaithmtan@gmail.com>
2019-11-18 21:09:53 -0800

    Change database name

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/restaurant_toilets', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/restaurooms', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
