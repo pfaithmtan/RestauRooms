94bcc88534f59acad9728c83b86ae08f280535d7
Faith <pfaithmtan@gmail.com>
2019-11-19 11:47:30 -0800

    Create one file for styling
    - Rename file

---
+++ client/styling/style.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+    background: #d4af37;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    justify-content: space-around;
+    color: white
+    background: black;
+`;
+
+export const Reviews = styled.div`
+    display: flex;
+    justify-content: center;
+    color: white;
+    background: blue;
+`;
