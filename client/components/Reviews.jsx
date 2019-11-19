de3680d94ab5d84e5b82ecd6326caeb7a543dd33
Faith <pfaithmtan@gmail.com>
2019-11-19 13:17:42 -0800

    Enable typing into review text box
    - Handle submit by creating alert (does not post)

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -8,7 +8,19 @@ class Reviews extends React.Component {
         this.state = {
             review: ''
         };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({ review: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert(`A review for ${this.props.name}'s restroom was submitted`);
+        event.preventDefault();
+      }
 
     render() {
         const { review } = this.state;
@@ -22,10 +34,10 @@ class Reviews extends React.Component {
                         <div>{review.rating}</div>
                     </OneReview>)
                 }
-                <form >
+                <form onSubmit={this.handleSubmit}>
                     <label>
                         Add a review!
-                      <input name="review" value={review} />
+                      <input type="text" value={review} onChange={this.handleChange} />
                     </label>
                     <input type="submit" value="Add" />
                 </form>
