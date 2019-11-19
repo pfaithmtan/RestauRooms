7285e3019bd295b9831db95a882079c869729486
Faith <pfaithmtan@gmail.com>
2019-11-18 22:52:52 -0800

    Change reviews key to align with schema

--- db/dummyData.js
+++ db/dummyData.js
@@ -3,7 +3,7 @@ module.exports = [
         id: 1,
         name: 'Lao Table',
         address: '149 2nd St, San Francisco, CA 94105',
-        reviews: [
+        toiletReviews: [
             {
                 rating: 5,
                 review: "Their bathroom was LIT. You can have a party in here. SO good, love it."
