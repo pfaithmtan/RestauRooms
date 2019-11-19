e11f49835accb044e2369fe98668c4d6acd7b9f1
Faith <pfaithmtan@gmail.com>
2019-11-18 22:05:31 -0800

    Create dummy data to seed

---
+++ db/dummyData.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Lao Table',
+        address: '149 2nd St, San Francisco, CA 94105',
+        reviews: [
+            {
+                rating: 5,
+                review: "Their bathroom was LIT. You can have a party in here. SO good, love it."
+            },
+            {
+                rating: 4,
+                review: "Clean, hygienic, no paper towels."
+            }
+        ]
+    }, 
+];
