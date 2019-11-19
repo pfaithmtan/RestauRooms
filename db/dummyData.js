fbe6280f6f3737916782420b649b8ff34ca6e990
Faith <pfaithmtan@gmail.com>
2019-11-19 13:04:02 -0800

    Add two restaurants

--- db/dummyData.js
+++ db/dummyData.js
@@ -14,4 +14,42 @@ module.exports = [
             }
         ]
     }, 
+    {
+        id: 2,
+        name: 'The Bird',
+        address: '115 New Montgomery St, San Francisco, CA 94105',
+        toiletReviews: [
+            {
+                rating: 1,
+                review: "There's no bathroom here!"
+            },
+            {
+                rating: 1,
+                review: "Where the bathroom at?!"
+            },
+            {
+                rating: 1,
+                review: "Is there no bathroom..?"
+            },
+        ]
+    }, 
+    {
+        id: 3,
+        name: 'Tropisueno',
+        address: '75 Yerba Buena Ln, San Francisco, CA 94103',
+        toiletReviews: [
+            {
+                rating: 3,
+                review: "Decent bathroom, not too jank but nothing special."
+            },
+            {
+                rating: 4,
+                review: "Nice ambience, the dim lighting in the bathroom is such a mood. LOVE the sink, it's deep and that tap though, WOW she fancy!"
+            },
+            {
+                rating: 3,
+                review: "Spacey, not super clean but able to sit on the seat."
+            },
+        ]
+    }, 
 ];
