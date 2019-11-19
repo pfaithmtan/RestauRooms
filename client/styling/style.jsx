247adb544fc6942a75e0f4c365ab4eed0d85cb32
Faith <pfaithmtan@gmail.com>
2019-11-19 12:05:52 -0800

    Centre reviews div

--- client/styling/style.jsx
+++ client/styling/style.jsx
@@ -1,19 +1,20 @@
 import styled from 'styled-components';
 
 export const Main = styled.div`
-    background: #d4af37;
+    // background: #d4af37;
 `;
 
 export const Header = styled.div`
     display: flex;
     justify-content: space-around;
+    background: #d4af37;
     color: white
-    background: black;
 `;
 
-export const Reviews = styled.div`
-    display: flex;
-    justify-content: center;
+export const ReviewsDiv = styled.div`
+    margin: auto;
+    width: 60%;
+    padding: 10px;
     color: white;
     background: blue;
 `;
