78cc0b552d5dc1316115c579d786e2b9f015914e
Faith <pfaithmtan@gmail.com>
2019-11-19 00:14:08 -0800

    Render text to the DOM

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>HELLO THERE!</div>
+        );
+    }
+}
+
+export default App;
