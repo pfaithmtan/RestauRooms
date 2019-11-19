6e877dd3fe0e1ab0820af7d4f1fb5824102fb7a5
Faith <pfaithmtan@gmail.com>
2019-11-19 13:22:21 -0800

    Refactor component to be stateful

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
-const Search = (props) => (
-    <form >
-        <label>
-          Looking for a review?
-          <input name="review" placeholder="Search within reviews"/>
-        </label>
-        <input type="submit" value="Search" />
-      </form>
-);
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            address: '',
+            toiletReviews: []
+        };
+    }
+
+    render() {
+        return (
+            <form >
+                <label>
+                Looking for a review?
+                <input name="review" placeholder="Search within reviews"/>
+                </label>
+                <input type="submit" value="Search" />
+            </form>
+
+        );
+    }
+}
 
 export default Search;
