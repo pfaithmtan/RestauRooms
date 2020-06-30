c60e305ad25bcc23567859dd43f2485ad5718b87
Faith <pfaithmtan@gmail.com>
2020-06-30 16:30:13 -0700

    Create method to save specific restaurant to db

--- server/controllers/dbMethods.js
+++ server/controllers/dbMethods.js
@@ -1,28 +1,69 @@
 const db = require('../../db/index.js');
+const axios = require('axios');
+const config = require('../../config.js');
 
 module.exports = {
-    getRestaurant: (req, res) => {
-        const { id } = req.params;
+  saveRestaurantToDb: (req, res) => {
+    const { query } = req.query;
 
-        db.Restaurant.find({ id: id })
-            .then((data) => {
-                res.status(200).send(data);
-            })
-            .catch((err) => {
-                res.status(500).send(err);
-            });
-    },
-    saveToiletReview: (req, res) => {
-        const { id } = req.params;
+    axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json', {
+      params: {
+        query,
+        type: 'restaurant',
+        key: config.TOKEN,
+      }
+    })
+      .then((response) => {
+        const restaurantsArr = response.data.results;
 
-        const docToSave = {
-            rating: id.rating,
-            review: id.review
+        for (let i = 0; i < restaurantsArr.length; i++) {
+          const restaurant = new db.Restaurant(
+            {
+              place_id: restaurantsArr[i].place_id,
+              name: restaurantsArr[i].name,
+              address: restaurantsArr[i].formatted_address,
+              toiletReviews: [],
+            }
+          );
+
+          restaurant.save((err) => {
+            if (err) return handleError(err);
+            console.log(`Saved restaurant ${restaurantsArr[i].name} to db!`);
+          });
         }
 
-        const review = new db.Review(docToSave);
-        restaurant.save((err) => {
+        res.status(200).send(`Successfully saved ${restaurantsArr.length} restaurants to DB!`);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("DID NOT")
+        res.status(500).send(error);
+      })
+  },
 
-        });
+  getRestaurantFromDb: (req, res) => {
+    const { place_id } = req.params;
+
+    db.Restaurant.find({ place_id })
+      .then((data) => {
+        res.status(200).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  },
+
+  saveToiletReview: (req, res) => {
+    const { id } = req.params;
+
+    const docToSave = {
+      rating: id.rating,
+      review: id.review
     }
+
+    const review = new db.Review(docToSave);
+    restaurant.save((err) => {
+
+    });
+  }
 }
