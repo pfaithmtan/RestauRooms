5182b3406c410dd6a20996556de962f7db4698f2
Faith <pfaithmtan@gmail.com>
2020-06-30 16:22:28 -0700

    Seed db with 20 restaurants in SF as a start

--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,5 @@
-const data = require('./dummyData.js');
-const db = require('./index.js');
+const controllers = require('../server/controllers/controllers.js')
 
-for (let i = 0; i < data.length; i++) {
-    const restaurant = new db.Restaurant(data[i]);
-    restaurant.save((err) => {
-        if (err) return handleError(err);
-        // saved!
-        console.log('Saved to db!');
-    });
-}
+// start off database with 20 restaurants in SF
+
+controllers.get20RestaurantsInSF();
