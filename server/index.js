8969cae8786e54069a0329484744ac346b187276
Faith <pfaithmtan@gmail.com>
2020-06-30 16:28:30 -0700

    Remove GET request for getting restaurants from API as route
    - Include POST request to save restaurant to db

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./controllers/dbMethods.js');
-const controllers = require('./controllers/controllers.js');
 
 const app = express();
 const port = 8080;
@@ -11,8 +10,8 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/restaurant/:id', express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 
-app.get('/api/restaurant/:id', db.getRestaurant);
+app.get('/api/restaurant/:place_id', db.getRestaurantFromDb);
 app.post('/api/restaurant/:id', db.saveToiletReview);
-app.get('/api/restaurant/', controllers.getRestaurantsFromZomato);
+app.post('/api/restaurant', db.saveRestaurantToDb);
 
 app.listen(port, () => console.log(`RestauRooms server listening on port ${port}!`));
