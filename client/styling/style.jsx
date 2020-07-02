8c45e5418eb10bc71e8ceb3e59a0eba44d9ab0f6
Faith <pfaithmtan@gmail.com>
2020-07-01 18:01:17 -0700

    Add basic styling to distinguish divs
    - Refactor styling

--- client/styling/style.jsx
+++ client/styling/style.jsx
@@ -1,29 +1,40 @@
 import styled from 'styled-components';
 
 export const Main = styled.div`
-    // background: #d4af37;
+  background: #d6fff6;
+  display: flex;
+  flex-direction: column;
+  height: 665px;
+  font-family: Arial, Helvetica, sans-serif;
 `;
 
 export const Header = styled.div`
-    display: flex;
-    justify-content: space-around;
-    background: #d4af37;
-    color: #000000
+  background: linear-gradient(rgba(8,51,41,0), rgba(8,51,41,1)); //#083329;
+  display: flex;
+  justify-content: center;
+  margin: 5px;
+  padding: 10px;
+  font-size: 40px;
+  font-weight: bold;
+  color: white; //#1c5b4d;
 `;
 
-export const ReviewsDiv = styled.div`
-    margin: auto;
-    width: 60%;
-    padding: 10px;
-    color: white;
-    background: #696969;
+export const SearchAndReviewsDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
 `;
 
-export const OneReview = styled.div`
-    border-left-style: solid;
-    border-color: black;
-    border-width: 5px;
-    background: #DCDCDC;
-    color: black;
-    margin: 8px;
+export const SearchSide = styled.div`
+  background: #5ffcd7;
+  width: 100%;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+`;
+
+export const ReviewsSide = styled.div`
+  background: #22d8ae;
+  width: 100%;
+  margin: 10px;
 `;
