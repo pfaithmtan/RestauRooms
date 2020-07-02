319372663461cf1452b6818a6d26a37f57ec046a
Faith <pfaithmtan@gmail.com>
2020-07-01 18:00:55 -0700

    Refactor App to be functional component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,56 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import Search from './Search.jsx';
-import Reviews from './Reviews.jsx';
-import { Main, Header } from '../styling/style.jsx'
+// import Search from './Search.jsx';
+// import Reviews from './Reviews.jsx';
+import {
+  Main,
+  Header,
+  SearchAndReviewsDiv,
+  SearchSide,
+  ReviewsSide,
+} from '../styling/style'
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            name: '',
-            address: '',
-            toiletReviews: [],
-            filteredReviews: []
-        };
-    }
-
-    componentDidMount() {
-        this.getRestaurantToiletReview();
-    }
-
-    getRestaurantToiletReview() {
-        axios.get(`/api${window.location.pathname}`)
-            .then((response) => {
-                console.log(response.data);
-                this.setState({
-                    name: response.data[0].name,
-                    address: response.data[0].address,
-                    toiletReviews: response.data[0].toiletReviews
-                })
-            })
-            .catch((error) => {
-                console.log('Oh no! An error!');
-                console.log(error);
-            })
-    }
-
-    render() {
-        const { name, address, toiletReviews } = this.state;
-
-        return (
-            <Main>
-                <Header>
-                    <div> <h1> {name} </h1> </div>
-                    <div> <h3> {address} </h3> </div>
-                </Header>
-                <Search toiletReviews={toiletReviews}/>
-                <Reviews name={name} toiletReviews={toiletReviews}/>
-            </Main>
-        );
-    }
+const App = () => {
+  return (
+    <Main>
+      <Header>
+        RestauRooms
+      </Header>
+      <SearchAndReviewsDiv>
+        <SearchSide>
+          Search for restaurant
+        </SearchSide>
+        <ReviewsSide>
+          Reviews
+        </ReviewsSide>
+      </SearchAndReviewsDiv>
+    </Main>
+  );
 }
 
 export default App;
