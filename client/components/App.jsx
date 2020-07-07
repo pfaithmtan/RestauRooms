e8980b04fa9b8000285dad16cac22a5be2e821b9
Faith <pfaithmtan@gmail.com>
2020-07-07 14:26:20 -0700

    Get bathroom reviews from db of specific restaurant

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// import Search from './Search.jsx';
-// import Reviews from './Reviews.jsx';
 import {
   Main,
   Header,
   SearchAndReviewsDiv,
   SearchSide,
   ReviewsSide,
+  Review,
 } from '../styling/style'
+import Search from './Search'
 
 const App = () => {
+  const [reviews, setReviews] = useState([]);
+
+  const getReviews = (placeId) => {
+    axios.get(`/api/restaurant/${placeId}`)
+      .then((data) => {
+        setReviews(data.data[0].toiletReviews);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  const updateQuery = (query) => {
+    getReviews(query);
+  }
+
   return (
     <Main>
       <Header>
@@ -20,9 +36,22 @@ const App = () => {
       <SearchAndReviewsDiv>
         <SearchSide>
           Search for restaurant
+          <Search updateQuery={updateQuery} />
         </SearchSide>
         <ReviewsSide>
           Reviews
+          {
+            reviews.map((review) => (
+              <Review>
+                {
+                  review.review
+                }
+                {
+                  review.rating
+                }
+              </Review>
+            ))
+          }
         </ReviewsSide>
       </SearchAndReviewsDiv>
     </Main>
