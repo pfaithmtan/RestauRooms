e575aefdb31a6bc261445e8f4f91aefd8902b676
Faith <pfaithmtan@gmail.com>
2020-07-09 11:46:54 -0700

    Use currentPage props to style number with current page
    - Add hover to non current page numbers
    - Set background to blue when page number is clicked

--- client/components/Pagination.jsx
+++ client/components/Pagination.jsx
@@ -1,22 +1,47 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 
 import {
   PaginationDiv,
-  Numbers,
 } from '../styling/style.jsx';
 
-const Pagination = ({ reviewsPerPage, totalReviews, paginate }) => {
+const Numbers = styled.div`
+  height: 25px;
+  width: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  cursor: pointer;
+  background-color: #375E54;
+  color: #dddddd;
+  :hover {
+    background-color: #A9DBFF;
+    color: black;
+    font-weight: 900;
+  }
+  
+  ${(props) => (props.clickedPage)
+    && css`
+    background-color: #03A0DA;
+    pointer-events: none;
+    font-weight: 900;
+    color: white;
+  `}
+`;
+
+const Pagination = ({ reviewsPerPage, totalReviews, paginate, currentPage }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalReviews / reviewsPerPage); i++) {
     pageNumbers.push(i);
   }
 
-  return  (
+  return (
     <PaginationDiv>
       {
         pageNumbers.map((number) => (
-          <Numbers>
+          <Numbers clickedPage={number === currentPage} >
             <a onClick={() => paginate(number)}>{number}</a>
           </Numbers>
         ))
